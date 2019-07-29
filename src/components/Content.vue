81d984bd35c05b42d0104f6e56848583851ce7ad
Tomotaka Kato <kato.tomotaka@smileforce.jp>
2019-07-29 11:59:22 +0900

    TTSのAPIを直接キックするパターン作成

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -13,15 +13,36 @@
         ></v-text-field>
     </v-flex>
 
-    <v-flex xs5>
-        speechSynthesis
+    <v-flex xs10>
+        <v-layout row wrap >
+            <v-flex xs7>
+                speechSynthesis
+            </v-flex>
+
+            <v-flex xs3>
+                <v-btn fab small color=primary @click="blowserSpeak">
+                    <v-icon dark>volume_up</v-icon>
+                </v-btn>
+            </v-flex>
+        </v-layout>
+    </v-flex>
+            
+
+
+    <v-flex xs10>
+        <v-layout row wrap >
+            <v-flex xs7>
+                TTSのAPIを直接表示
+            </v-flex>
+
+            <v-flex xs3>
+                <v-btn fab small color=primary :href="getApiUrl()">
+                    <v-icon dark>volume_up</v-icon>
+                </v-btn>
+            </v-flex>
+        </v-layout>
     </v-flex>
 
-    <v-flex xs5>
-        <v-btn fab small color=primary @click="blowserSpeak">
-            <v-icon dark>volume_up</v-icon>
-        </v-btn>
-    </v-flex>
 
 
     </v-layout>
@@ -34,6 +55,7 @@ import { Vue, Component } from 'vue-property-decorator';
 @Component
 export default class Content extends Vue {
     private text: string = 'サンプルテキストです。';
+    private ttsApiUrl: string = 'https://arcane-temple-52272.herokuapp.com/?text=';
 
     private blowserSpeak() {
         try {
@@ -53,6 +75,14 @@ export default class Content extends Vue {
         } catch (_) {
             // AndroidのWebVew対応のためエラーは握りつぶす。
         }
+    }
+
+    private openTTSApiPage() {
+        window.location.href = this.getApiUrl();
+    }
+
+    private getApiUrl(): string {
+        return `${this.ttsApiUrl}${this.text}`
     }
 }
 </script>
